2631f176df942be658b7644254f63968c66303c5
Akshay Kamble <kamblea635@gmail.com>
2022-08-25 09:18:07 +0530

    first array example

---
+++ arrays/src/ArrayFirst.java
@@ -0,0 +1,8 @@
+public class ArrayFirst {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        int ros;
+        System.out.println(arr[0]);
+        System.out.println(ros);
+    }
+}
